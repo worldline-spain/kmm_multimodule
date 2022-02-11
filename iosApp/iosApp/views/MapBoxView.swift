4c6ed4366c262d909e42b2b8ad61c865b06297c7
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2022-02-11 15:28:44 +0100

    Show pois on map

--- iosApp/iosApp/views/MapBoxView.swift
+++ iosApp/iosApp/views/MapBoxView.swift
@@ -8,26 +8,80 @@
 
 import SwiftUI
 import MapboxMaps
+import PoiUI
 
-struct MapBoxMapView: UIViewControllerRepresentable {
-     
-    func makeUIViewController(context: Context) -> MapViewController {
-           return MapViewController()
-       }
-      
-    func updateUIViewController(_ uiViewController: MapViewController, context: Context) {
+class PoiMapProxy: ObservableObject {
+    var viewModel = PoiListViewModel()
+    
+    @Published var state: PoiListState = PoiListState.InProgress()
+    
+    init() {
+        viewModel.observe(viewModel.state) { newState in
+            self.state = newState as! PoiListState
+        }
     }
 }
 
-class MapViewController: UIViewController {
-   internal var mapView: MapView!
-   override public func viewDidLoad() {
-       super.viewDidLoad()
-       let myResourceOptions = ResourceOptions(accessToken: "")
-       let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions)
-       
-       mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
-       mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-       self.view.addSubview(mapView)
-   }
+struct MapBoxMapView: UIViewControllerRepresentable {
+    
+    @ObservedObject var proxy = PoiListProxy()
+    
+    func makeUIViewController(context: Context) -> MapViewController {
+        proxy.viewModel.onEvent(event: PoiListEvent.Attach())
+        return MapViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: MapViewController, context: Context) {
+        uiViewController.updateState(newState: proxy.state)
+    }
+    
 }
+
+class MapViewController: UIViewController {
+    
+    internal var mapView: MapView!
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let myResourceOptions = ResourceOptions(accessToken: "")
+        let cameraOptions = CameraOptions(center: CLLocationCoordinate2D(latitude: 40.463667, longitude: -3.74922), zoom: 4.5, bearing: 0, pitch: 0)
+        
+        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: cameraOptions)
+        
+        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(mapView)
+        
+    }
+    
+    func updateState(newState: PoiListState){
+        if newState is PoiListState.InProgress {
+            print("In progress")
+        } else if newState is PoiListState.Error {
+            print("Error")
+        } else if newState is PoiListState.Success {
+            if let response = newState as? PoiListState.Success {
+                print("Points \(response.pois)")
+                showPoints(pois: response.pois)
+            }
+        }
+    }
+    
+    func showPoints(pois: [CorePoi]){
+        
+        var locations : [PointAnnotation] = []
+        for poi in pois {
+            var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: poi.latitude, longitude: poi.longitude))
+            
+            pointAnnotation.image = .init(image: UIImage(systemName: "pin.fill")!, name: "pin.fill")
+            pointAnnotation.iconAnchor = .bottom
+            locations.append(pointAnnotation)
+        }
+        
+        let pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
+        pointAnnotationManager.annotations = locations
+    }
+}
+
