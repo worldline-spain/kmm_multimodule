0a5e28a71de88ec4b5e7469f13d32568435d1184
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2022-02-21 17:42:36 +0100

    Refactor

--- androidApp/src/main/java/com/worldline/kmm/android/ui/composables/home/HomeRoute.kt
+++ androidApp/src/main/java/com/worldline/kmm/android/ui/composables/home/HomeRoute.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
@@ -12,9 +11,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.worldline.kmm.android.R
 import com.worldline.kmm.android.exhaustive
 import com.worldline.kmm.android.ui.composables.poilist.PoiListRoute
 import com.worldline.kmm.android.ui.composables.poilist.stateWithLifecycle
